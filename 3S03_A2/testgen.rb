073c7bc11b62bc2e2f669b6d2503ba23e9256df4
Connor Hallett <connor.hallett@gmail.com>
2015-03-21 17:59:38 -0400

    Added test files and completed testgen.rb

--- 3S03_A2/testgen.rb
+++ 3S03_A2/testgen.rb
@@ -1,50 +1,64 @@
-outfiles = ["test1.txt" , "test2.txt" , "test3.txt" , "test4.txt"]
+outfiles = ["test1.txt" , "test3.txt" , "test4.txt"]
 
 outfiles.each do |i|
-  infile = File.open(i, "w")
-  if (i == "test1.txt")
+  infile = File.open i, "w"
+  if i == "test1.txt"
     25.times do
       nextline = "t,e,"
       j = rand(20)
       j.times do
         nextline += "A"
+      end
       k = rand(20)
       k.times do
         nextline += "D"
+      end
       z = rand(20)
       z.times do
         nextline += "W"
+      end
       nextline += ",s,D"
       infile.puts nextline
-  else if (i == "test2.txt")
+    end
+  elsif i == "test2.txt"
 
-  else if (i == "test3.txt")
+  elsif i == "test3.txt"
       51.times do
         nextline = "t,e,"
         j = rand(20)
         j.times do
           nextline += "A"
+        end
         k = rand(20)
         k.times do
           nextline += "D"
+        end
         z = rand(20)
         z.times do
           nextline += "W"
+        end
         nextline += ",s,D"
         infile.puts nextline
-  else if (i == "test4.txt")
+      end
+  elsif (i == "test4.txt")
       50.times do
         nextline = "t,e,"
         j = rand(20)
         j.times do
           nextline += "A"
+        end
         k = rand(20)
         k.times do
           nextline += "D"
+        end
         z = rand(20)
         z.times do
           nextline += "W"
+        end
         nextline += ",s,D"
         infile.puts nextline
+      end
   infile.close
+end
 puts("done")
+end
